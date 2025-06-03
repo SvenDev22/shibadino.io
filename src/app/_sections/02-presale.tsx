e9645e2609ec48c97b776cfe41846e151f48f74e
Quang Tuyen <tuyenbq.dev@gmail.com>
2025-06-03 23:08:25 +0700

    remove space

--- src/app/_sections/02-presale.tsx
+++ src/app/_sections/02-presale.tsx
@@ -607,7 +607,7 @@ function Widget() {
       const tier: any = getTradingNotification(
         Number(toToken) / Number(presaleData?.oneUsdPrice),
       );
-      let message = `${tier.message} \n\n${tier.iconRepresentation}\n\n`;
+      let message = `${tier.message} \n\n${tier.iconRepresentation}\n`;
       message += `\n🦖 ${new Intl.NumberFormat("en-US").format(toToken)} DINO`;
       message += `\n💰 Paid: ${from} ${tokenType == 1 ? "SOL" : "USDT"}`;
       message += `\n🔄 Worth: $${new Intl.NumberFormat("en-US").format(Number(toToken) / Number(presaleData?.oneUsdPrice))}`;
